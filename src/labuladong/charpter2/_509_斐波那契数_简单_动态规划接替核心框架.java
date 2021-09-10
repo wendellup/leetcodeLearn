1bd8d4538aff8383920e088c8bbbc92b13072836
wendellup <wendellup@163.com>
2021-09-10 14:05:48 +0800

    '完成322_零钱兑换'

--- src/labuladong/charpter2/_509_斐波那契数_简单_动态规划接替核心框架.java
+++ src/labuladong/charpter2/_509_斐波那契数_简单_动态规划接替核心框架.java
@@ -4,7 +4,7 @@ package labuladong.charpter2;
  * 
  * @author wendellup 参考https://labuladong.gitee.io/algo/3/24/60/
  */
-public class _509_斐波那契数_简单 {
+public class _509_斐波那契数_简单_动态规划接替核心框架 {
 	int fib(int n) {
 		if (n < 1)
 			return 0;
@@ -17,5 +17,18 @@ public class _509_斐波那契数_简单 {
 			curr = sum;
 		}
 		return curr;
+	}
+	
+	public int fib2(int n) {
+	    if (n == 0) return 0;
+	    int[] dp = new int[n + 1];
+	    // base case
+	    dp[0] = 0; dp[1] = 1;
+	    // 状态转移
+	    for (int i = 2; i <= n; i++) {
+	        dp[i] = dp[i - 1] + dp[i - 2];
+	    }
+
+	    return dp[n];
 	}
 }
